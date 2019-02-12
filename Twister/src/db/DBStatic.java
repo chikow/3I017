77796d505ed82582dce9fc970240e84ca89e93e0
Walid <walid.chiko@gmail.com>
2019-02-12 12:31:55 +0100

    OK

---
+++ Twister/src/db/DBStatic.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package db;
+
+/**
+ * @author LAOUER Walid
+ *
+ */
+
+	public class DBStatic {
+		
+		public static String mysql_host = "sql111.epizy.com";
+		public static String mysql_db = "epiz_23443993_Twister";
+		public static String mysql_username = "epiz_23443993";
+		public static String mysql_password = "Twister19";
+		public static boolean is_pooling = false;
+		
+	/*
+	 * Un pooling c'est comme une boite , il ne ferme pas vraiment pour eviter qu'à
+	 * chaqiue fois on doit ouvrir/fermer la connexion
+	 */		
+}
