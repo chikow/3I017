420c1a2b3e77545eebdb8d0d0241695fdbf8ab40
Nail <>
2019-02-17 23:13:42 +0100

    test succeeded

--- Twister/src/db/Database.java
+++ Twister/src/db/Database.java
@@ -9,30 +9,37 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 public class Database {
-	private static Database database;
+	private static Database database=null;
 	private DataSource dataSource;
 	
 	/**
-	 * @param jndiname
-	 * @throws SQLException
+	 * Constructeur d'une base de données.
+	 * @param name Le nom du driver pour établir la connexion.
+	 * @throws SQLException.
 	 */
-	public Database(String jndiname) throws SQLException {
+	public Database(String name) throws SQLException {
 		try {
-			dataSource = (DataSource) new InitialContext().lookup("java:comp/env/" + jndiname);
+			dataSource = (DataSource) new InitialContext().lookup("java:comp/env/" + name);
 		} catch (NamingException e) {
 			// Handle error that it's not configured in JNDI.
-			throw new SQLException(jndiname + " is missing in JNDI! : " + e.getMessage());
+			throw new SQLException(name + " is missing in JNDI! : " + e.getMessage());
 		}
 	}
 	
 	/**
-	 * @return
-	 * @throws SQLException
+	 * Établir une connexion.
+	 * @return La connexion établie.
+	 * @throws SQLException.
 	 */
 	public Connection getConnection() throws SQLException {
 		return dataSource.getConnection();
 	}
 	
+	/**
+	 * Établis une connexion vers notre base de données MySQL.
+	 * @return La connexion MySQL établie.
+	 * @throws SQLException.
+	 */
 	public static Connection getMySQLConnection() throws SQLException {
 		if(!DBStatic.is_pooling){
 			return(DriverManager.getConnection("jdbc:mysql://" + DBStatic.mysql_host + "/" + DBStatic.mysql_db, DBStatic.mysql_username, DBStatic.mysql_password));
---
+++ Twister/src/tools/Data.java
@@ -0,0 +1,20 @@
+package tools;
+/**
+ * Class des constatntes utilisées dans l'application
+ * */
+public class Data {
+	public static int CODE_MISSING_PARAMETERS = -1;
+	public static String MESSAGE_MISSING_PARAMETERS = "Missing parameter(s).";
+	public static int CODE_USER_DOES_NOT_EXIST = 1003;
+	public static String MESSAGE_USER_DOES_NOT_EXIST = "USER does not exist.";
+	public static int CODE_FRIEND_DOES_NOT_EXIST = 1002;
+	public static String MESSAGE_FRIEND_DOES_NOT_EXIST = "FRIEND does not exist.";
+	public static int CODE_INCORRECT_LOGIN_PASSWORD = 1004;
+	public static String MESSAGE_INCORRECT_LOGIN_PASSWORD = "Incorrect login/password.";
+	public static int CODE_ERROR_JSON = 101;
+	public static String MESSAGE_ERROR_JSON = "Error JSON.";
+	public static int CODE_USER_ALREADY_CONNECTED = 1005;
+	public static String MESSAGE_USER_ALREADY_CONNECTED = "User already connected.";
+
+
+}
