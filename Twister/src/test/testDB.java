8e47c3db571bee51137057d066010c71d0823246
Walid <walid.chiko@gmail.com>
2019-02-19 14:02:20 +0100

    ok

--- Twister/src/test/testDB.java
+++ Twister/src/test/testDB.java
@@ -19,8 +19,6 @@ public class testDB {
 public static void main(String[] args) {
 	
 	//JSONObject obj = CreatUserS.createUser("chikow","Walid", "Laouer", "walid@live.fr", "mdp");
-	
-	//JSONObject obj = CreatUserS.createUser(login, nom, prenom, mail, mdp)
 	//System.out.println(obj.toString());
 	System.out.println(UserTools.checkPassword("chikow", "mdp"));
 	
